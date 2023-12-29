bf59cdcbe8d7f0e2879b8bf9fcf4d9878559ce1c
ting <ting04180@gmail.com>
2023-12-29 16:57:38 +0800

    加上html跟index.js加root

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
